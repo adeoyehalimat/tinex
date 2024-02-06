0d3392a820fd24a033431bcdb63dfb2ead5041fb
Adeoye Halimat <adeoyehalimah@gmail.com>
2024-02-05 23:45:10 -0800

    initial commit: modified about.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,13 +3,9 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script setup>
+</script>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
